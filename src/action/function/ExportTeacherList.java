2ff1c79181b7ca408ad1bc7b013474556ef245e2
LionCherry <hiwanglu@foxmail.com>
2017-12-04 17:07:07 +0800

    指导教师名单

---
+++ src/action/function/ExportTeacherList.java
@@ -0,0 +1,85 @@
+package action.function;
+
+import java.io.*;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import action.*;
+import obj.*;
+
+/**
+ * 导出实习生名单
+ */
+public class ExportTeacherList extends ActionSupport{
+	private static final long serialVersionUID = 3677055466118899859L;
+
+	private action.Annual annual=new action.Annual();
+	public action.Annual getAnnual(){return this.annual;}
+	
+
+	static public final String SessionListKey=Export.SessionListKey; 
+	
+	public ExportTeacherList(){
+		super();
+	}
+
+	private String jumpURL="function_Export_display.action";
+		public String getJumpURL() {return this.jumpURL;}
+		public void setJumpURL(String a) {this.jumpURL=a;}
+
+	@Override
+	public String execute(){
+		return Manager.tips("该项目不可用!","jump");
+	}
+
+	/*
+	 * 下载模板
+	 */
+	private String downloadFileName;
+		public void setDownloadFileName(String a){
+			this.downloadFileName=a;
+			try{this.downloadFileName=new String(a.getBytes("gb2312"), "iso8859-1");
+			}catch(UnsupportedEncodingException e){
+				e.printStackTrace();
+				this.downloadFileName=a;
+			}//*/
+		}
+		public String getDownloadFileName(){return this.downloadFileName;}
+	private ByteArrayOutputStream downloadOutputStream=null;
+	protected String downloadByIO(SpecialIO io,int year,OutputStream stream) throws IOException{
+		return io.createTeacherList(year,stream);
+	}
+	public String download(){//下载模板
+		System.out.println(">> ExportStudentList:download >");
+		System.out.println(">> ExportStudentList:download > create download file.");
+		this.downloadOutputStream=new ByteArrayOutputStream();
+		try{
+			String fileName=this.downloadByIO((SpecialIO)Base.io(),
+					this.getAnnual().getYear(),downloadOutputStream);
+			this.setDownloadFileName(fileName);//设置下载文件名称
+			this.downloadOutputStream.flush();
+		}catch(IOException e){
+			this.downloadOutputStream=null;
+			return Manager.tips("服务器开小差去了，暂时无法下载！",e,"jump");
+		}
+		System.out.println(">> ExportStudentList:download <downloadAttachment");
+		return "downloadAttachment";
+	}
+	public InputStream getDownloadAttachment(){//实际上获取的输出流，使用getter获取的downloadAttachment
+		ByteArrayInputStream in=IOHelper.ByteArrayOutStream2InputStream(this.downloadOutputStream);
+		try {
+			this.downloadOutputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	/*	try {
+			ServletActionContext.getResponse().setHeader("Content-Disposition","attachment;downloadFileName="+java.net.URLEncoder.encode(this.downloadFileName, "UTF-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}//*/
+		return in;
+	}
+	
+
+
+}
