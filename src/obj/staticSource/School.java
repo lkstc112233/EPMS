54e7d3f39bcbf79befea80334f5b41b3abeb3a73
LionCherry <hiwanglu@foxmail.com>
2017-12-08 16:26:09 +0800

    新增学院School表新列subName表示简称

--- src/obj/staticSource/School.java
+++ src/obj/staticSource/School.java
@@ -8,12 +8,16 @@ import obj.*;
 public class School extends Base{
 	@SQLField(value="顺序号",weight=0)
 	private int orderId=-1;	public void setOrderId(int a){this.orderId=a;}	public void setOrderId(String a) {this.orderId=Field.s2i(a,-1);}	public int getOrderId() {return this.orderId;}
-	
+
 	@SQLField(value="学院名称",weight=1,isKey=true,notNull=true)
 	private String name;
-	
+	@SQLField(value="简称",weight=10,notNull=true)
+	private String subName="";//TODO
+
 	public String getName(){return this.name;}
 	public void setName(String a){this.name=Field.s2S(a);}
+	public String getSubName(){return this.subName;}
+	public void setSubName(String a){this.subName=Field.s2S(a);}
 	
 	public School(){
 		super();
